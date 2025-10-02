ca05f718852c9bfa34f9de91a10e2b2a903aafd9
nagma <bano.nagma.1@gmail.com>
2025-10-02 18:37:37 +0530

    inline styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,11 @@ export default function Header() {
     <header>
       <img src={logo} alt="A canvas" />
       <h1>ReactArt</h1>
-      <p>A community of artists and art-lovers.</p>
+      <p style={{
+        color: 'red',
+        textAlign: 'center',
+        margin: 0
+       }}>A community of artists and art-lovers.</p>
     </header>
   );
 }
