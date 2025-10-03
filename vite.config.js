de48043d08bbfc661e3bae0bef84fc5db4c0ba35
nagma <bano.nagma.1@gmail.com>
2025-10-03 21:49:51 +0530

    setting up existing google font library

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,14 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  content: ["index.html","./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        title: ['"Pacifico"', 'cursive'],
+      },
+    },
+  },
   plugins: [react(), tailwindcss()],
+  
 })
