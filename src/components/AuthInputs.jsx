0a091febd2f138ff883bbfcce5d0b019276508cb
nagma <bano.nagma.1@gmail.com>
2025-10-03 13:22:45 +0530

    Flexible components with styled components

--- src/components/AuthInputs.jsx
+++ src/components/AuthInputs.jsx
@@ -20,6 +20,17 @@ const Label = styled.label`
       color: #6b7280;
   `;
 
+const Input = styled.input`
+      width: 100%;
+      padding: 0.75rem 1rem;
+      line-height: 1.5;
+      background-color: #d1d5db;
+      color: #374151;
+      border: 1px solid transparent;
+      border-radius: 0.25rem;
+      box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06);
+`;
+
 export default function AuthInputs() {
   const [enteredEmail, setEnteredEmail] = useState('');
   const [enteredPassword, setEnteredPassword] = useState('');
@@ -47,7 +58,7 @@ export default function AuthInputs() {
           <Label className={`label ${emailNotValid ? "invalid" : ""}`}>
             Email
           </Label>
-          <input
+          <Input
             type="email"
             // style={{
             //   backgroundColor: emailNotValid ? 'salmon' : 'transparent',
@@ -60,7 +71,7 @@ export default function AuthInputs() {
           <Label className={`label ${emailNotValid ? "invalid" : ""}`}>
             Password
           </Label>
-          <input
+          <Input
             type="password"
             // style={{
             //   backgroundColor: emailNotValid ? 'salmon' : 'transparent',
