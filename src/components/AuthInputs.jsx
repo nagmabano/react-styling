bb173be8ac2ccc4fedc430a4dd5644fbbc23ddbb
nagma <bano.nagma.1@gmail.com>
2025-10-03 22:42:04 +0530

    using tailwind css

--- src/components/AuthInputs.jsx
+++ src/components/AuthInputs.jsx
@@ -5,13 +5,13 @@ import Input from "./Input.jsx";
 
 
 // tagged template literal within backticks
-const ControlledContainer = styled.div`
-                              display: flex;
-                              flex-direction: column;
-                              gap: 0.5rem;
-                              margin-bottom: 1.5rem;
-                            }
-                          `;
+// const ControlledContainer = styled.div`
+//                               display: flex;
+//                               flex-direction: column;
+//                               gap: 0.5rem;
+//                               margin-bottom: 1.5rem;
+//                             }
+//                           `;
 
 export default function AuthInputs() {
   const [enteredEmail, setEnteredEmail] = useState('');
@@ -34,8 +34,8 @@ export default function AuthInputs() {
   const passwordNotValid = submitted && enteredPassword.trim().length < 6;
 
   return (
-    <div id="auth-inputs">
-      <ControlledContainer>
+    <div id="auth-inputs" className="w-full max-w-sm p-8 mx-auto rounded shadow-md bg-gradient-to-b from-stone-700 to-stone-800">
+      <div className="flex flex-col gap-2 mb-6">
           <Input
             label="Email"
             type="email"
@@ -56,9 +56,9 @@ export default function AuthInputs() {
               handleInputChange('password', event.target.value)
             }
           />
-      </ControlledContainer>
-      <div className="actions">
-        <button type="button" className="text-button">
+      </div>
+      <div className="actions flex justify-end gap-4">
+        <button type="button" className="text-amber-400 hover:text-amber-500 hover:underline">
           Create a new account
         </button>
         <Button className='button' onClick={handleLogin}>Sign In</Button>
--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -5,7 +5,7 @@ export default function Input({label,invalid, ...props}) {
     labelClasses += " text-red-400";
     inputClasses += " text-red-500 bg-red-100 border-red-300";
   } else {
-    labelClasses += " text-stone-700";
+    labelClasses += " text-stone-300";
     inputClasses += " text-gray-700 bg-stone-300";
   }
 
